78aa26d3bcf6d645b58588ec52cf4cdaacb0fef8
CharlesGarrocho <charles.garrocho@gmail.com>
2012-12-28 17:54:18 -0200

    adicionado: nova classe que ira tratar os métodos da api.

--- source/botwipy.py
+++ source/botwipy.py
@@ -8,26 +8,48 @@ import time
 import tweepy
 import settings
 
-# Adicionado as chaves no oauth.
-auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
-auth.set_access_token(settings.OAUTH_TOKEN, settings.OAUTH_TOKEN_SECRET)
+class BotAPI(tweepy.API):
 
-# Conectando a API utilizando os dados da aplicação.
-api = tweepy.API(auth)
+    def __init__(self, auth):
+        super(BotAPI, self).__init__(auth)
 
-print 'Meus Dados'
-print 'Nome: {0}'.format(api.me().name)
+    def get_meu_nome(self):
+        return self.me().name()
 
-mensagens = tweepy.Cursor(api.user_timeline).items()
+    def get_meus_tweets(self):
+        return tweepy.Cursor(api.user_timeline).items()
 
-print '\nMinhas Mensagens'
-for status in mensagens:
-    print status.text
+    def get_amigos_tweets(self):
+        return api.friends_timeline()
 
-# Verificando as mensagens com Via referência e adicionando esses usuários.
-msm_amigos = api.friends_timeline()
-for mensagem in msm_amigos:
-    if '( via @' in mensagem.text:
-        usuario = re.findall(r'via @(.*?)\)', mensagem.text)[0]
-        api.get_user(usuario).follow()
-        print '{0} começou a seguir {1}'.format(api.me().name, usuario)
+    def seguir_usuario(self, usuario):
+        self.get_user(usuario).follow()
+
+    def verifica_tweet(self, tweet, condicao):
+        usuario = re.findall(r'{0}(.*?):'.format(condicao), tweet.text)
+        if (len(usuario) > 0):
+            return usuario[0]
+        return None
+
+
+if __name__ == '__main__':
+
+    # Adicionado as chaves no oauth.
+    print 'Definindo Chaves de Acesso do BOT...'
+    auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
+    auth.set_access_token(settings.OAUTH_TOKEN, settings.OAUTH_TOKEN_SECRET)
+
+    # Conectando a API utilizando os dados da aplicação.
+    print 'Autenticando BOT no AUTH...'
+    api = BotAPI(auth)
+
+    # Verificando as mensagens com Via referência e adicionando esses usuários.
+    print 'Entrando no Loop para Verificar Novos Tweets'
+    while True:
+        time.sleep(2)
+        amigos_tweets = api.get_amigos_tweets()
+        for tweet in amigos_tweets:
+            usuario = api.verifica_tweet(tweet, 'RT @')
+            if usuario is not None:
+                api.seguir_usuario(usuario)
+                print '{0} começou a seguir {1}'.format(api.me().name, usuario)
