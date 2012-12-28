cccdd20a7794d4ecba1b87eec195521498174949
CharlesGarrocho <charles.garrocho@gmail.com>
2012-12-28 14:51:47 -0200

    corrigido: acesso de leitura para acesso total a conta.

---
+++ source/conecta.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# @author: Charles Tim Batista Garrocho
+# @contact: charles.garrocho@gmail.com
+# @copyright: (C) 2012-2013 Python Software Open Source
+
+import time
+import tweepy
+import settings
+
+# Adicionado as chaves no oauth.
+auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
+auth.set_access_token(settings.OAUTH_TOKEN, settings.OAUTH_TOKEN_SECRET)
+
+# Conectando a API utilizando os dados da aplicação.
+api = tweepy.API(auth)
+
+print 'Nome: {0}'.format(api.me().name)
+api.update_status('Testando Python Twitter')
