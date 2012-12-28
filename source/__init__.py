ba664523cf48d6b59a37225fb90af12ea51ce564
CharlesGarrocho <charles.garrocho@gmail.com>
2012-12-28 14:14:54 -0200

    adicionado: arquivo de configurações da api OAUTH

---
+++ source/__init__.py
@@ -0,0 +1,20 @@
+# @author: Charles Tim Batista Garrocho
+# @contact: ctgarrocho@gmail.com
+# @copyright: (C) 2012-2012 Python Software Open Source
+
+"""
+Este é o pacote do projeto SEMON.
+"""
+
+
+__version__ = '0.1D.1'
+
+
+__all__ = [
+    'detector',
+    'gui',
+    'handler',
+    'recursos',
+    'servidor',
+    'settings',
+]
---
+++ source/settings.py
@@ -0,0 +1,11 @@
+# @author: Charles Tim Batista Garrocho
+# @contact: charles.garrocho@gmail.com
+# @copyright: (C) 2012-2013 Python Software Open Source
+
+# Consumer Key
+CONSUMER_KEY = 'RLidpawnpYvY6X92xk58hg'
+CONSUMER_SECRET = 'WLm6c5muTUza294yNNRmiSaXAVIoe3KrVQ5EPL3lA'
+
+# Access Token
+OAUTH_TOKEN = '1024579182-5QEmN8w53JpUPCTSPSX26PpupUAreaRgDBIbi0K'
+OAUTH_TOKEN_SECRET = 'fZKEFjrvHi5l3PofZlgG9icWZWOIhdutPV2Urd4HK8s'
