65e2a400380cc8540537a668d905a75d57371ae2
CharlesGarrocho <charles.garrocho@gmail.com>
2012-12-29 16:47:06 -0200

    adicionado: novos botoes para interação ao bot.

--- source/gui.py
+++ source/gui.py
@@ -20,19 +20,43 @@ class Example(QtGui.QMainWindow):
         tableWidget.addLinha(["Gustavo Garrocho", "Caramba em so tem kiter...", "10:53:38"])
         self.setCentralWidget(tableWidget)
 
-        exitAction = QtGui.QAction(QtGui.QIcon('./icons/exit24.png'), 'Sair', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Sair da Aplicacao')
-        exitAction.triggered.connect(self.close)
+        self.iniciarBot = QtGui.QAction(QtGui.QIcon('./icons/start24.png'), 'Iniciar', self)
+        self.iniciarBot.setShortcut('Ctrl+I')
+        self.iniciarBot.setStatusTip('Iniciar o Bot - Ctrl+I')
+        self.iniciarBot.triggered.connect(self.close)
+        
+        self.pararBot = QtGui.QAction(QtGui.QIcon('./icons/end24.png'), 'Parar', self)
+        self.pararBot.setShortcut('Ctrl+P')
+        self.pararBot.setStatusTip('Parar o Bot - Ctrl+P')
+        self.pararBot.triggered.connect(self.close)
+        
+        self.confBot = QtGui.QAction(QtGui.QIcon('./icons/edit24.png'), 'Configurar', self)
+        self.confBot.setShortcut('Ctrl+C')
+        self.confBot.setStatusTip('Configurar o Bot - Ctrl+C')
+        self.confBot.triggered.connect(self.close)
+        
+        self.ajuda = QtGui.QAction(QtGui.QIcon('./icons/help24.png'), 'Ajuda', self)
+        self.ajuda.setShortcut('Ctrl+H')
+        self.ajuda.setStatusTip('Ajuda do Bot - Ctrl+H')
+        self.ajuda.triggered.connect(self.close)
+        
+        self.sair = QtGui.QAction(QtGui.QIcon('./icons/exit24.png'), 'Sair', self)
+        self.sair.setShortcut('Ctrl+Q')
+        self.sair.setStatusTip('Sair da Aplicacao - Ctrl+Q')
+        self.sair.triggered.connect(self.close)
 
         self.statusBar()
 
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&Arquivo')
-        fileMenu.addAction(exitAction)
+        #menubar = self.menuBar()
+        #fileMenu = menubar.addMenu('&Arquivo')
+        #fileMenu.addAction(self.exitAction)
 
-        toolbar = self.addToolBar('Sair')
-        toolbar.addAction(exitAction)
+        toolBar = self.addToolBar('Sair')
+        toolBar.addAction(self.iniciarBot)
+        toolBar.addAction(self.pararBot)
+        toolBar.addAction(self.confBot)
+        toolBar.addAction(self.ajuda)
+        toolBar.addAction(self.sair)
         
         self.setGeometry(600, 600, 650, 550)
         self.setWindowTitle('BOTWIPY - Bot em Python Para Twitter')
