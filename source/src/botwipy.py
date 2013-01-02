e1d7284813ec6751efdc890ffa6f05028fc80265
CharlesGarrocho <charles.garrocho@gmail.com>
2013-01-01 22:36:07 -0200

    corrigido: métodos de busca de nome e verificação de usuarios para seguir.

--- source/src/botwipy.py
+++ source/src/botwipy.py
@@ -11,25 +11,29 @@ import settings
 
 class BotAPI(tweepy.API):
 
-    def __init__(self, auth):
-        super(BotAPI, self).__init__(auth)
+    def __init__(self):
         self.RODAR = settings.RODAR
 
         # Adicionado as chaves no oauth.
         auth = tweepy.OAuthHandler(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
         auth.set_access_token(settings.OAUTH_TOKEN, settings.OAUTH_TOKEN_SECRET)
+        super(BotAPI, self).__init__(auth)
         
     def get_meu_nome(self):
-        return self.me().name()
+        return self.me().name
 
     def get_meus_tweets(self):
-        return tweepy.Cursor(api.user_timeline).items()
+        return tweepy.Cursor(self.user_timeline).items()
 
     def get_amigos_tweets(self):
-        return api.friends_timeline()
+        return self.friends_timeline()
 
     def seguir_usuario(self, usuario):
-        self.get_user(usuario).follow()
+        try:
+            self.get_user(usuario).follow()
+            return '{0} comecou a seguir {1}'.format(self.get_meu_nome(), usuario)
+        except:
+            return '{0} nao conseguiu seguir {1}'.format(self.get_meu_nome(), usuario)
 
     def verifica_tweet(self, tweet, condicao):
         usuario = re.findall(r'{0}'.format(condicao), tweet.text)
