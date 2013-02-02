4ef0858081a8f83f47b0f745bdfde2108487a67a
CharlesGarrocho <charles.garrocho@gmail.com>
2013-02-02 17:37:01 -0200

    adicionado: documentação nos modulos, classes e metodos do botwipy.py

--- source/src/botwipy.py
+++ source/src/botwipy.py
@@ -4,6 +4,10 @@
 # @contact: charles.garrocho@gmail.com
 # @copyright: (C) 2012-2013 Python Software Open Source
 
+"""
+Modulo responsavel pelas interacoes com a api do tweepy no software.
+"""
+
 import re
 import time
 import tweepy
@@ -14,6 +18,9 @@ from redis import Redis
 
 
 class BotAPI():
+    """
+    Classe responsavel por tratar as interacoes do bot.
+    """
 
     def __init__(self):
         self.RODAR = True
@@ -34,6 +41,10 @@ class BotAPI():
         self.api = tweepy.API(auth)
 
     def carrega_api(self):
+        """
+        Carrega as chaves oauth e estabiliza um acesso com a api.
+        Retorna verdadeiro caso as chaves estejam corretas, ou falso, caso contrario.
+        """
         auth = tweepy.OAuthHandler(self.redis.get('CONSUMER_KEY'), self.redis.get('CONSUMER_SECRET'), self.redis.get('OAUTH_TOKEN'))
         auth.set_access_token(self.redis.get('OAUTH_TOKEN'), self.redis.get('OAUTH_TOKEN_SECRET'))
         self.api = tweepy.API(auth)
@@ -44,21 +55,37 @@ class BotAPI():
             return True
 
     def get_meu_nome(self):
+        """
+        Retorna o nome da conta que foi configurada.
+        """
         try:
             return self.api.me().name
         except:
             return None
         
     def get_meu_status(self):
+        """
+        Retorna o status atual da conta que foi configurada.
+        """
         return self.api.get_status(self.api.me().id).text
 
     def get_meus_tweets(self):
+        """
+        Retorna uma lista das ultimas 20 mensagens da conta que foi configurada.
+        """
         return tweepy.Cursor(self.api.user_timeline).items()
 
     def get_amigos_tweets(self):
+        """
+        Retorna uma lista das ultimas 20 mensagens dos amigos da conta que foi configurada.
+        """
         return self.api.friends_timeline()
 
     def seguir_usuario(self, usuario):
+        """
+        Segue um novo usuario a partir do parametro passado. Retorna uma mensagem
+        de confirmacao, caso ele tenha consiguido ou nao seguir o usuario.
+        """
         try:
             self.api.get_user(usuario).follow()
             return 'comecou a seguir {0}'.format(usuario)
@@ -66,12 +93,19 @@ class BotAPI():
             return 'nao conseguiu seguir {0}'.format(usuario)
 
     def verifica_tweet(self, tweet, condicao):
+        """
+        Analiza a mensagem, verificando se existe algum usuario.
+        Se existe um usuario, ele o retorna, caso contrario, retorna None.
+        """
         usuario = re.findall(r'{0}'.format(condicao), tweet.text)
         if (len(usuario) > 0):
             return [usuario[0], tweet.created_at]
         return None
 
     def get_seguidores(self):
+        """
+        Retorna uma lista com todos os seguidores da conta configurada.
+        """
         seguidores = tweepy.Cursor(self.api.followers, id = self.api.me().id)
         lista = []
         for seguidor in seguidores.items():
@@ -79,6 +113,9 @@ class BotAPI():
         return lista
 
     def get_mensoes(self):
+        """
+        Obtem uma lista de todos as mensoes existentes no twitter em uma lista.
+        """
         minhas_mensoes = tweepy.Cursor(self.api.mentions).items()
         lista = []
         for status in minhas_mensoes:
@@ -86,9 +123,15 @@ class BotAPI():
         return lista
 
     def send_mensagem(self, usuario, mensagem):
+        """
+        Envia uma mensagem para um determinado usuario.
+        """
         self.api.send_direct_message(user_id = usuario, text = mensagem)
 
     def atualizar_status(self, mensagem):
+        """
+        Atualizar o Status da conta que foi configurada.
+        """
         try:
             self.api.update_status(mensagem)
         except:
